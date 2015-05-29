4b2117615566bf42c021e1f17927b048aebb8106
Qingye Jiang (John) <qjiang@ieee.org>
2015-05-29 02:22:38 +0000

    S3 demo

--- src/main/java/net/qyjohn/aws/DemoS3.java
+++ src/main/java/net/qyjohn/aws/DemoS3.java
@@ -20,8 +20,12 @@ public class DemoS3
 	 *
 	 */
 
-	public void upload(String bucketName, String key, File file) throws Exception
+	public void upload(String bucketName, String fileName) throws Exception
 	{
+		// Extract the key name from filename
+		File file = new File(fileName);
+		String key = file.getName();
+
 		// Open a file input stream from the file object.
 		FileInputStream inputStream = new FileInputStream(file);
 
@@ -36,32 +40,7 @@ public class DemoS3
 
 	/**
 	 *
-	 * This method creates a presigned URL for a specific S3 object.
-	 *
-	 */
-
-	public URL getPresignedURL(String bucketName, String key, int minutes)
-	{
-		// Set expiration date of the presigned URL
-		java.util.Date expiration = new java.util.Date();
-		long msec = expiration.getTime();
-		msec += 1000 * 60 * minutes;
-		expiration.setTime(msec);
-			     
-		// Prepare to generate the presigned URL
-		GeneratePresignedUrlRequest generatePresignedUrlRequest = 
-		      new GeneratePresignedUrlRequest(bucketName, key);
-		generatePresignedUrlRequest.setMethod(HttpMethod.GET); 
-		generatePresignedUrlRequest.setExpiration(expiration);
-
-		// Generate the presigned URL
-		URL url = s3client.generatePresignedUrl(generatePresignedUrlRequest); 
-		return url;
-	}
-
-	/**
-	 *
-	 * This demo uploads a file to a specify bucket, then returns a presigned URL for the uploaded object.
+	 * This demo uploads a file to a specify bucket.
 	 *
 	 */
 
@@ -74,14 +53,8 @@ public class DemoS3
 			String fileName   = args[1];	
 				
 			// Upload to S3
-			File file = new File(fileName);
-			String keyName = file.getName();
 			DemoS3 demo = new DemoS3();
-			demo.upload(bucketName, keyName, file);
-			     
-			// Generate the presigned URL which will expire in 5 minutes
-			URL url = demo.getPresignedURL(bucketName, keyName, 5); 
-			System.out.println(url);		
+			demo.upload(bucketName, fileName);
 	        } catch (Exception e) 
 		{
 			// Simple exception handling by printing out error message and stack trace
